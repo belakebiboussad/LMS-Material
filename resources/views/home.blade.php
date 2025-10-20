5c4a80de812c413b23bb19440a00c6534003dd3f
belakebi <bbelakebi@cdta.dz>
2025-10-20 23:09:08 +0100

    seed dairas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,74 @@
-@extends('layouts.app')
-@section('content')
+<html>
+<head>
+      <!-- Bootstrap Core Css -->
+    <link href="{{ asset('theme/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="{{ asset('theme/plugins/node-waves/waves.css') }}" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="{{ asset('theme/plugins/animate-css/animate.css') }}" rel="stylesheet" />
+    <!-- Morris Chart Css-->
+
+    <link href="{{asset('theme/plugins/morrisjs/morris.css')}}" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="{{ asset('theme/css/style.css') }}" rel="stylesheet">
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="{{ asset('theme/css/themes/all-themes.css') }}" rel="stylesheet" />
+     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
 <div class="container">
-  
+ <div class="input-group input-group-static mb-4">
+                <label for="exampleFormControlSelect1" class="ms-0">Example select</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
 </div>
-@endsection
+
+<script src="{{ asset('theme/plugins/jquery/jquery.min.js')}}"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="{{ asset('theme/plugins/bootstrap/js/bootstrap.js') }}"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="{{ asset('theme/plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
+   
+    <!-- Slimscroll Plugin Js -->
+    <script src="{{ asset('theme/plugins/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+   
+    <!-- Waves Effect Plugin Js -->
+    <script src="{{ asset('theme/plugins/node-waves/waves.js') }}"></script>
+   
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="{{ asset('theme/plugins/jquery-countto/jquery.countTo.js') }}"></script>
+    <!-- Morris Plugin Js -->
+    <script src="{{ asset('theme/plugins/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/morrisjs/morris.js') }}"></script>
+    <script src="{{ asset('theme/plugins/morrisjs/morris.js') }}"></script>
+     <!-- ChartJs -->
+    <script src="{{ asset('theme/plugins/chartjs/Chart.bundle.js') }}"></script>
+    <!-- Flot Charts Plugin Js -->
+    <script src="{{ asset('theme/plugins/flot-charts/jquery.flot.js') }}"></script>
+    
+    <script src="{{ asset('theme/plugins/flot-charts/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('theme/plugins/flot-charts/jquery.flot.pie.js') }}"></script>
+    
+    <script src="{{ asset('theme/plugins/flot-charts/jquery.flot.categories.js') }}"></script>
+    
+    <script src="{{ asset('theme/plugins/flot-charts/jquery.flot.time.js') }}"></script>
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="{{ asset('theme/plugins/jquery-sparkline/jquery.sparkline.js') }}"></script>
+
+    <!-- Custom Js -->
+
+
+</body>
+</html>
+
