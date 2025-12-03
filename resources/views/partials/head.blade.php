0e3d6f473729dd36b18c06750fd425617dbdea51
belakebi <bbelakebi@cdta.dz>
2025-12-03 12:10:36 +0100

    add nucleo css file

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,5 @@
     <!-- Bootstrap Core Css -->
-    <link href="{{ asset('theme/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
-
+    <link href="{{ asset('theme/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Waves Effect Css -->
     <link href="{{ asset('theme/plugins/node-waves/waves.css') }}" rel="stylesheet" />
 
@@ -10,12 +9,16 @@
 
     <link href="{{asset('theme/plugins/morrisjs/morris.css')}}" rel="stylesheet" />
 
-    <!-- Custom Css -->
-    <link href="{{ asset('theme/css/style.css') }}" rel="stylesheet">
-
-    <link href="{{ asset('theme/css/themes/all-themes.css') }}" rel="stylesheet" />
+      <link href="{{ asset('theme/css/themes/all-themes.css') }}" rel="stylesheet" />
     <link href="{{ asset('theme/css/material.indigo-pink.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <!-- <link href="{{ asset('theme/css/material-dashboard.css') }}" rel="stylesheet">-->
+    <!-- <link href="{{-- asset('theme/css/material-dashboard.css') --}}" rel="stylesheet">-->
+    {{-- test --}}
+    <link href="{{ asset('theme/css/nucleo-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/css/nucleo-svg.css') }} rel="stylesheet" />
+
+    {{-- fin test --}}
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="{{ asset('css/leaflet.css') }}" rel="stylesheet">
+      <!-- Custom Css -->
+    <link href="{{ asset('theme/css/style.css') }}" rel="stylesheet">
