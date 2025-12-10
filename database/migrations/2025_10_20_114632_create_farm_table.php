a34ed8441307b08aba261e7248d818f9170ca51c
belakebi <bbelakebi@cdta.dz>
2025-12-10 16:07:51 +0100

    migration and farm nin reference

--- database/migrations/2025_10_20_114632_create_farm_table.php
+++ database/migrations/2025_10_20_114632_create_farm_table.php
@@ -17,14 +17,17 @@ return new class extends Migration
             $table->char('recordNbr',10);
             $table->string('name', 255)->nulable();
             $table->decimal('area', 10, 2)->unsigned()->nullable();
-            $table->string('owner_id', 20)->constrained('users','NIN')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('owner_id',20);
+            $table->string('guardien_id',20)->nullable();
             $table->decimal('y_lat', 13, 11)->nullable();
             $table->decimal('x_lon', 14, 11)->nullable();
             $table->string('address',100)->nulable();
             $table->unsignedTinyInteger('wilaya_id')->nullable();
             $table->string('phone',10)->nulable();
+            $table->foreign('owner_id')->references('NIN')->on('users')->onUpdate('cascade')->onDelete('cascade');
            // $table->foreignId('owner_id')->constrained('users','NIN')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('guardien_id')->nullable()->constrained('users','NIN')->onUpdate('cascade')->onDelete('cascade');
+            //$table->foreignId('guardien_id')->nullable()->constrained('users','NIN')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('guardien_id')->references('NIN')->on('users')->nullable()->onUpdate('cascade')->onDelete('cascade');
            
             $table->date('creationDt');         
             $table->timestamps();
